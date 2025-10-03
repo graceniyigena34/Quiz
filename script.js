72406996bca2e964c88ef0a28a1a63494aea808d
graceniyigena34 <graceniyigena34@gmail.com>
2025-10-03 10:16:29 +0200

    I added the display product for user

--- script.js
+++ script.js
@@ -36,26 +36,49 @@ displayAllAuthors();
 
 
 
-// Question 2
+// // Question 2
 
-async function fetchDataForAllAuthors(){
-    try{
+// async function fetchDataForproducts(){
+//     try{
 
-        // make an api request 
-const response= await response (`https://dummyjson.com/products`);
+//         // make an api request 
+// const response= await response (`https://dummyjson.com/products/${displayproduct}`);
+// // check if the response is successful 
+// if (!response.ok){
+//     throw new error(`HTTP.error!status:${response.error
+//     }`);
+// }
+// // to transform data into json format 
+// const data = await response.json
+// return data;
 
-if (!response.ok){
-    throw new error(`HTTP.error!status:${response.error
-    }`);
-}
 
 
 
+//     }catch (error){
+//         console.log("Error fetching user data:", error);
+//     }
+// }
 
-    }catch (error){
-        const
-    }
-}
+// async function displayproduct(){
+//      const posts = await fetchDataForAllProducts();
+
+//   if (posts && posts.length > 0) {
+//     posts.forEach((post) => {
+//       console.log("Post id:", post.id);
+//       console.log("Product id:", post.product.id);
+//       console.log("Product title:", post.product.title);
+//       console.log("Price (title):", post.title);
+//       console.log("Comments:", post.comments.length);
+//       console.log("----");
+//     });
+//   } else {
+//     console.log("No posts found");
+//   }
+// }
+
+// displayproducts();
+
 
 
 
